f90b6f237e31b4c4ed119970f89ebe23f21926a1
Loïc Feuga <loic.feuga@gmail.com>
2022-12-09 17:21:01 +0100

    Résolution Day 9 Part 1

--- src/9decembre/grid.model.ts
+++ src/9decembre/grid.model.ts
@@ -1,23 +1,16 @@
 import {Point} from "./point.model";
+import {Corde} from "./corde.model";
 
 export class Grid {
     points: Array<Point> = [];
+    corde: Corde = new Corde(0, 0);
 
     placeCordeInitial(x: number, y: number) {
         let p: Point = {
-            bothPassed: false,
-            hPassed: true,
-            tPassed: false,
-            x: 0,
-            y: 0,
-            label: 'H',
+            bothPassed: true, hPassed: true, label: "T", tPassed: true, x: 0, y: 0
         }
         this.points.push(p);
-        this.placeCordeT();
-    }
-
-    moveCorde() {
-
+        // this.placeCordeT();
     }
 
     placeCordeT() {
@@ -31,176 +24,107 @@ export class Grid {
 
     }
 
-
-    async moveCordeD(distance: number) {
-        for (let i = 0; i < distance; i++) {
-            let h = this.getHPoint();
-            let t = this.getTPoint();
-
-            let newH = {...h};
-            let nPh = this.getDotPoint(newH.x,newH.y);
-
-            h.label = h.bothPassed ? '#' : '.';
-            newH.y--;
-
-            newH.label = 'H';
-            newH.tPassed =  nPh ? nPh.tPassed : false;
-            newH.bothPassed =  nPh ? nPh.bothPassed : false;
-            this.points.push(newH);
-
-            if (Math.max(newH.y, t.y) > (Math.min(newH.y, t.y) + 1)) {
-                let newT = {...t};
-                if (t.hPassed && t.tPassed) {
-                    t.bothPassed = true;
-                    t.label = '#';
-
-                }
-
-
-                newT.y--;
-                if(newT.x != newH.x) {
-                    newT.x = newH.x;
-                }
-                newT.tPassed = true;
-                this.points.push(newT);
-            }
-
-            this.clearGrid();
-        }
-    }
-
-    async moveCordeU(distance: number) {
-        for (let i = 0; i < distance; i++) {
-            let h = this.getHPoint();
-            let t = this.getTPoint();
-
-            let newH = {...h};
-
-            h.label = h.bothPassed ? '#' : '.';
-            newH.y++;
-            newH.label = 'H';
-            let nPh = this.getDotPoint(newH.x,newH.y);
-            newH.tPassed =  nPh ? nPh.tPassed : false;
-            newH.bothPassed =  nPh ? nPh.bothPassed : false;
-            this.points.push(newH);
-
-            if (Math.max(newH.y, t.y) > (Math.min(newH.y, t.y) + 1)) {
-                let newT = {...t};
-                if (t.hPassed && t.tPassed) {
-                    t.bothPassed = true;
-                    t.label = '#';
-                }
-                newT.y++;
-                if(newT.x != newH.x) {
-                    newT.x = newH.x;
-                }
-                newT.tPassed = true;
-
-                this.points.push(newT);
-            }
-
-            this.clearGrid();
-
-        }
-    }
-
-    async moveCordeR(distance: number) {
-
-        for (let i = 0; i < distance; i++) {
-            let h = this.getHPoint();
-            let t = this.getTPoint();
-
-            let newH = {...h};
-
-            h.label = h.bothPassed ? '#' : '.';
-            newH.x++;
-            newH.label = 'H';
-            let nPh = this.getDotPoint(newH.x,newH.y);
-            newH.tPassed =  nPh ? nPh.tPassed : false;
-            newH.bothPassed =  nPh ? nPh.bothPassed : false;
-            this.points.push(newH);
-
-            if (Math.max(newH.x, t.x) > (Math.min(newH.x, t.x) + 1)) {
-                let newT = {...t};
-                if (t.hPassed && t.tPassed) {
-                    t.bothPassed = true;
-                    t.label = '#';
-                }
-
-                newT.x++;
-                if(newT.y != newH.y) {
-                    newT.y = newH.y;
-                }
-                newT.tPassed = true;
-                this.points.push(newT);
-            }
-
-            this.clearGrid();
-
-        }
-
-    }
-
     moveCorde(direction: string, distance: number) {
 
         for (let k = 0; k < distance; k++) {
-            let h = this.getHPoint();
-            let t = this.getTPoint();
 
-        }
-    }
+            switch (direction) {
+                case 'U':
+                    this.corde.yH++;
+                    if (Math.max(this.corde.yH, this.corde.yT) > (Math.min(this.corde.yH, this.corde.yT) + 1)) {
+                        this.corde.yT++;
+                        if (this.corde.xT != this.corde.xH) {
+                            this.corde.xT = this.corde.xH;
+                        }
+                    }
+                    break;
+                case 'R':
+                    this.corde.xH++;
+                    if (Math.max(this.corde.xH, this.corde.xT) > (Math.min(this.corde.xH, this.corde.xT) + 1)) {
+                        this.corde.xT++;
+                        if (this.corde.yT != this.corde.yH) {
+                            this.corde.yT = this.corde.yH;
+                        }
+                    }
+                    break;
+                case 'L':
+                    this.corde.xH--;
+                    if (Math.max(this.corde.xH, this.corde.xT) > (Math.min(this.corde.xH, this.corde.xT) + 1)) {
+                        this.corde.xT--;
+                        if (this.corde.yT != this.corde.yH) {
+                            this.corde.yT = this.corde.yH;
+                        }
+                    }
+                    break;
+                case 'D':
+                    this.corde.yH--;
+                    if (Math.max(this.corde.yH, this.corde.yT) > (Math.min(this.corde.yH, this.corde.yT) + 1)) {
+                        this.corde.yT--;
+                        if (this.corde.xT != this.corde.xH) {
+                            this.corde.xT = this.corde.xH;
+                        }
+                    }
+                    break;
+            }
 
-    async moveCordeL(distance: number) {
-
-        for (let i = 0; i < distance; i++) {
-            let h = this.getHPoint();
-            let t = this.getTPoint();
-
-            let newH = {...h};
-
-            h.label = h.bothPassed ? '#' : '.';
-            newH.x--;
-            newH.label = 'H';
-
-            let nPh = this.getDotPoint(newH.x,newH.y);
-            newH.tPassed =  nPh ? nPh.tPassed : false;
-            newH.bothPassed =  nPh ? nPh.bothPassed : false;
-            this.points.push(newH);
-
-            if (Math.max(newH.x, t.x) > (Math.min(newH.x, t.x) + 1)) {
-                let newT = {...t};
-                if (t.hPassed && t.tPassed) {
-                    t.bothPassed = true;
-                    t.label = '#';
-
+            let nextH = this.getDotPoint(this.corde.xH, this.corde.yH);
+            if (nextH) {
+                nextH.hPassed = true;
+            } else {
+                this.points.push({bothPassed: false, hPassed: true, label: "", tPassed: false, x: this.corde.xH, y: this.corde.yH})
+            }
+            let nextT = this.getDotPoint(this.corde.xT, this.corde.yT);
+            if (nextT) {
+                nextT.tPassed = true;
+                if(nextT.hPassed) {
+                    nextT.bothPassed = true;
                 }
-
-                newT.x--;
-                if(newT.y != newH.y) {
-                    newT.y = newH.y;
-                }
-                newT.tPassed = true;
-                this.points.push(newT);
+            } else {
+                this.points.push({bothPassed: false, hPassed: true, label: "", tPassed: true, x: this.corde.xT, y: this.corde.yT})
             }
 
 
-            this.clearGrid();
         }
-
     }
 
     clearGrid() {
         let dot = this.points.filter(p => p.label !== '.' && p.bothPassed);
-        for(let dd of dot){
-            let index = this.points.findIndex(p => p.x === dd.x && p.y === dd.y && p.label === "." );
-            if(index > -1) {
-                this.points.splice(index,1);
-                this.clearGrid();
+        let f = false;
+        for (let dd of dot) {
+            let index = this.points.findIndex(p => p.x === dd.x && p.y === dd.y && p.label === ".");
+            if (index > -1) {
+                this.points.splice(index, 1);
+                f = true;
             }
         }
+        if (f) {
+
+            this.clearGrid();
+        }
     }
-    findIndexOf(p: Point) {
-        return this.points.findIndex(p => p === p);
+
+    async render() {
+        let yM: Array<any> = this.points.map(p => p.y);
+        let yMax = Math.max(...yM);
+        let xM: Array<any> = this.points.map(p => p.x);
+        let xMax = Math.max(...xM);
+
+        for (let y = yMax; y >= 0; y--) {
+            let l = this.points.filter(p => p.y === y).sort((a, b) => {
+                return a.x < b.x ? -1 : 1;
+            });
+            let line = '';
+            for (let i = 0, ii = 0; i <= xMax; i++) {
+                if ((l[ii]?.x === i && l[ii]?.y === y)) {
+                    line += l[ii].bothPassed ? '#' : '.';
+                    ii++;
+                } else {
+                    line += '.';
+                }
+            }
+            console.log(line);
+        }
+
     }
 
     private getHPoint(): Point {
@@ -213,30 +137,6 @@ export class Grid {
 
     private getDotPoint(x: number, y: number): Point | undefined {
         return this.points.find(p => p.y === y && p.x === x);
-    }
-
-    async render() {
-        let yM: Array<any> = this.points.map(p => p.y);
-        let yMax = Math.max(...yM);
-        let xM: Array<any> = this.points.map(p => p.x);
-        let xMax = Math.max(...xM);
-
-        for(let y = yMax; y >= 0; y--){
-            let l = this.points.filter(p => p.y === y).sort((a,b) => {
-                return a.x < b.x ? -1 : 1;
-            });
-            let line = '';
-            for(let i = 0, ii = 0; i <= xMax;i++){
-                if((l[ii]?.x === i && l[ii]?.y ===y )){
-                    line+=l[ii].bothPassed ? '#' : l[ii].label;
-                    ii++;
-                } else {
-                    line+='.';
-                }
-            }
-            console.log(line);
-        }
-
     }
 
 }
